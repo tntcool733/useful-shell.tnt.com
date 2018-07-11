592cd6b7682d6ea5b5d4974aba3e443343fd602a
tntcool733 <tntcool733@163.com>
2018-07-11 17:39:57 +0800

    批量查询进程pid

--- get_pid.sh
+++ get_pid.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# 说明：此脚步用于批量获取某父目录下，子目录名称的进程pid
+# 例子：bash ./get_pid.sh /data/service/java_base www-data
+
+# 需要遍历查找的父目录
 parent_dir=$1
+# 需要过滤的进程用户id
 process_uid=$2
 
 dirs=$(ls -l $parent_dir |awk '/^d/ {print $NF}')
